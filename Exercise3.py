ce7517dac7819b0cafe3b19232bca2a2e603779e
Ерохин Максим <110667386+Makas888@users.noreply.github.com>
2022-08-17 14:37:52 +0300

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,9 +1,9 @@
 #  1. Write a Python program to print the number entered by user only if the number entered is negative.
-number = float(input('entered number: '))
+number = float(input('enter the number: '))
 print('number is positive:', number >= 0)
 
 #  2. Write a Python program to check if the value a is less than 20 or not.
-value = float(input('entered value: '))
+value = float(input('enter the value: '))
 print('value < 20 :', not value > 20)
 
 #  3. Write a Python program to check if a given number is Zero or Not.
@@ -11,9 +11,9 @@ given_number = float(input('number: '))
 print('number is zero:', given_number == 0 or given_number < 0 < given_number)
 
 #  4. Write a Python program to check if a given number is Even or Odd.
-odd_even = float(input('entered number: '))
+odd_even = float(input('enter the number: '))
 print('number is even:', odd_even % 2 == 0)
 
 #  5. Write a Python program to find largest number among three numbers entered by user.
-numbers = input('entered 3 numbers: ')
+numbers = input('enter 3 numbers: ')
 print('entered numbers', numbers, 'equal numbers:', max(list(numbers)))
