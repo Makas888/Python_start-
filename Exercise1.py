18fd23ca3f726a04b76d1e7c2f0cf2d8a2449039
Ерохин Максим <110667386+Makas888@users.noreply.github.com>
2022-08-17 15:11:26 +0300

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -2,18 +2,18 @@ print('Hello, World!')
 
 print('Hello, World!', 'Python - super!', 'See you...', sep='\n')
 
-length_rectangle = int(input('длина прямоугольника: '))
-width_rectangle = int(input('ширина прямоугольника: '))
+length_rectangle = int(input('length rectangle: '))
+width_rectangle = int(input('width rectangle: '))
 area_rectangle = length_rectangle * width_rectangle
-print('площадь прямоугольника = ', area_rectangle)
+print('area rectangle = ', area_rectangle)
 
 number1 = int(input('enter first number: '))
 number2 = int(input('enter second number: '))
-sum = number1 + number2
+summa = number1 + number2
 product = number1 * number2
 difference = number1 - number2
 quotient = number1 / number2
-print(sum, product, difference, quotient)
+print(summa, product, difference, quotient)
 
 radius_of_a_circle = int(input('radius of a circle: '))
 r = radius_of_a_circle
