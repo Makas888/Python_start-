93fbc8e9c6c17c008feff1b6f05bdc6cba15e141
Ерохин Максим <110667386+Makas888@users.noreply.github.com>
2022-09-07 12:51:10 +0300

    Update Exercise10.py

--- Exercise10.py
+++ Exercise10.py
@@ -37,14 +37,16 @@ def count_word(text):
 # большой палиндром, полученный умножением двух трехзначных чисел.
 # Выведите значение этого палиндрома и то, произведением каких чисел он
 # является.
-def palindrome(a, b):
+def palindrome(def palindrome(a, b):
     palindromes = []
     for j in range(a, 1, -1):
         for i in range(b, 1, -1):
             if str(j * i) == str(j * i)[::-1]:
                 palindromes.append([j * i, j, i])
     res = max(palindromes)
-    return f'{res[0]} = is max palindrome of {res[1]} and {res[2]}'
+    return res
 
 
+result = palindrome(999, 999)
+print(f'{result[0]} - is max palindrome of {result[1]} and {result[2]}')
 print(palindrome(999, 999))
